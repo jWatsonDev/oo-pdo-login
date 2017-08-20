30ea81472e44a1225349b10399a12a3428dda53b
Jay Watson <jaywatson1982@gmail.com>
2017-08-20 10:21:41 +0000

    add profile view

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ $user = new User();
 
 if ($user->isLoggedIn()) {
 ?>
-    <p>Hello <a href="#"><?= escape($user->data()->username); ?></a>!</p>
+    <p>Hello <a href="profile.php?user=<?= escape($user->data()->username); ?>"><?= escape($user->data()->username); ?></a>!</p>
     <ul>
         <li><a href="logout.php">Log out</a></li>
         <li><a href="update.php">Update</a></li>
